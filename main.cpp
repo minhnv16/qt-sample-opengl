242d3761247b71cb1f59249dcc1e4ea3a72d4301
minhnv16 <nguyenminhasp@gmail.com>
2021-05-19 11:44:19 +0700

    Fix CmakeFile

--- main.cpp
+++ main.cpp
@@ -17,13 +17,41 @@ int main()
 {
     WindowCreator a;
 
-    cout << "Hello GL!" << endl;
+    cout << "Hello G1L!" << endl;
 
 
-//    glfwInit();
-//    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-//    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-//    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+
+    GLFWwindow* window = 0;
+    if (GLFW_FALSE == glfwInit()) {
+        return -1;
+    }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    cout << "glfwWindowHint" << endl;
+
+    int nWidth = 800, nHeight = 600;
+    //get_resolution(nWidth, nHeight);
+    window = glfwCreateWindow(nWidth, nHeight, "Test OpenGL", NULL /* glfwGetPrimaryMonitor()*/, NULL);
+
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+    else{
+        cout << "glfwCreateWindow OK" << endl;
+    }
+
+    GLenum err = glewInit();
+
+    if (GLEW_OK != err) {
+        glfwTerminate();
+        cout << "GLEW_OK != err" <<err<< endl;
+        return -1;
+    }
+    cout << "GLEW_OK" << endl;
 
 
     return 0;
